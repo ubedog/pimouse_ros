f3bd82828a9e46fec4f74fefcc7f1c2aa110b89f
Kaoru Akira <ubedog180416@gmail.com>
2019-01-18 11:18:55 +0900

    Add files

--- travis_package_make.bash
+++ travis_package_make.bash
@@ -1,12 +1,6 @@
 #!/bin/bash -xve
 
 #sync and make
-rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
-
-
-cd ~/catkin_ws/src/
-git clone --depth=1 http://github.com/ubedog/pimouse_ros.git
-
-
+rsync -av ./ ~/catkin_ws/src/pimose_ros/
 cd ~/catkin_ws
 catkin_make
